e8dd2f8549e07a95f9a0d785e89f2c78c3553e3d
Nikolay Mahotkin <nikolay.makhotkin@gmail.com>
2017-08-03 16:30:07 +0300

    Update run_tasks.py

--- src/run_tasks.py
+++ src/run_tasks.py
@@ -2,5 +2,21 @@ import os
 
 from mlboardclient.api import client
 
-ml = client.Client()
 
+SUCCEEDED = 'Succeeded'
+FAILED = 'Failed'
+
+
+def main():
+    ml = client.Client()
+
+    current_task_name = os.environ.get('TASK_NAME')
+    current_app = os.environ['APP_NAME']
+
+    app = ml.apps.get(current_app)
+    
+    for task in app.tasks:
+        
+
+if __name__ == '__main__':
+    main()
